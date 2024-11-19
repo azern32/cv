665a08d54e6ddab9223fb21b031658ad912bae8f
azern32 <chiro.emiya@gmail.com>
2024-11-19 14:08:05 +0700

    persiapan cv pake flowbite

--- dev/src/routes/+page.svelte
+++ dev/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import { PUBLIC_TEST } from '$env/static/public';
+</script>
+
+<h1>Welcome to {PUBLIC_TEST}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- dev/svelte.config.js
+++ dev/svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+// import autoadapter from '@sveltejs/adapter-auto';
+import bunadapter from 'svelte-adapter-bun'
+import staticadapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: process.env.NODE_ENV === "production" ? staticadapter() : bunadapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+		}
 	}
 };
 
